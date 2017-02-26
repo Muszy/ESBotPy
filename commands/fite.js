76b0d67f737f327df93684ab32d8ab9c4a3b68fe
Hani <hanlu.wang711@gmail.com>
2017-02-26 18:37:51 -0500

    added five

---
+++ commands/fite.js
@@ -0,0 +1,18 @@
+var request = require("superagent");
+const discord = require("discord.js");
+
+var fite = ["(ง ͠° ͟ل͜ ͡°)ง", "ʕง•ᴥ•ʔง", "(ง’̀-‘́)ง", "(งಠ_ಠ)ง", "(งಠل͜ಠ)ง", "(ง ° ͜ ʖ °)ง"]
+
+exports.run = (bot, msg, args) => {
+
+    let embed = new discord.RichEmbed();
+    embed.setColor(0xFFB6C1)
+        .setDescription(fite[Math.floor(Math.random() * (fite.length))]);
+
+    msg.channel.sendEmbed(embed).catch(console.error);
+
+}
+
+exports.help = (bots, msg, args) => {
+    return "fite me";
+}
