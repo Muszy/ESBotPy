fa9f933a5bb442f8f726c8d9bdb018244cc705bc
Hani <hanlu.wang711@gmail.com>
2017-03-03 23:20:57 -0500

    your placement in ranks

--- commands/rank.js
+++ commands/rank.js
@@ -16,7 +16,7 @@ exports.run = (bot, msg, args) => {
     }
 
     if (args.length == 1) {
-    	if (args[0].toLowerCase() == "rep") {
+        if (args[0].toLowerCase() == "rep") {
             guildSortRep(bot, msg, "Server", "Rep");
             return;
         }
@@ -37,34 +37,51 @@ exports.run = (bot, msg, args) => {
         }
         if (args[0].toLowerCase() == "global" && args[1].toLowerCase() == "rep") {
             let rep = [];
+            let userPlace, userRep;
             for (var id in userSettings) {
                 rep.push([id, userSettings[id].rep]);
             }
             rep.sort(function(a, b) {
                 return b[1] - a[1]
             });
-            genList(bot, msg, "Global", "Rep", rep.splice(0, 10));
+
+            for (var num in rep) {
+                if (rep[num][0] == msg.author.id) {
+                    userPlace = parseInt(num) + 1;
+                    userRep = rep[num][1];
+                }
+            }
+            genList(bot, msg, "Global", "Rep", rep.splice(0, 10), userPlace, userRep);
             return;
         }
     }
 
     let cash = [];
+    let userPlace, userCash;
     for (var id in userSettings) {
         cash.push([id, userSettings[id].dia]);
     }
     cash.sort(function(a, b) {
         return b[1] - a[1]
     });
+
+    for (var num in cash) {
+        if (cash[num][0] == msg.author.id) {
+            userPlace = parseInt(num) + 1;
+            userCash = cash[num][1];
+        }
+    }
     //console.log(cash.splice(0, 10));
-    genList(bot, msg, "Global", "Cash", cash.splice(0, 10));
+    genList(bot, msg, "Global", "Cash", cash.splice(0, 10), userPlace, userCash);
 }
 
 exports.help = (bots, msg, args) => {
     return "To get the top 10 rankings, you can use `!rank [server/global] [rep/cash]`.\nThe default is rankings for global cash.";
 }
 
-function guildSort(bot, msg, type, cred, gID) {
+function guildSort(bot, msg, type, cred) {
     let users = [];
+    let place, dia;
     for (var help in userSettings) {
         if (msg.guild.member(help) != null) {
             users.push([help, userSettings[help].dia]);
@@ -73,12 +90,20 @@ function guildSort(bot, msg, type, cred, gID) {
     users.sort(function(a, b) {
         return b[1] - a[1]
     });
+
+    for (var id in users) {
+        if (users[id][0] == msg.author.id) {
+            place = parseInt(id) + 1;
+            dia = users[id][1];
+        }
+    }
     //console.log(users.splice(0, users.length));
-    genList(bot, msg, type, cred, users.splice(0, users.length));
+    genList(bot, msg, type, cred, users.splice(0, users.length), place, dia);
 }
 
-function guildSortRep(bot, msg, type, cred, gID) {
+function guildSortRep(bot, msg, type, cred) {
     let users = [];
+    let place, rep;
     for (var help in userSettings) {
         if (msg.guild.member(help) != null) {
             users.push([help, userSettings[help].rep]);
@@ -87,71 +112,78 @@ function guildSortRep(bot, msg, type, cred, gID) {
     users.sort(function(a, b) {
         return b[1] - a[1]
     });
+
+    for (var id in users) {
+        if (users[id][0] == msg.author.id) {
+            place = parseInt(id) + 1;
+            rep = users[id][1];
+        }
+    }
     //console.log(users.splice(0, users.length));
-    genList(bot, msg, type, cred, users.splice(0, users.length));
+    genList(bot, msg, type, cred, users.splice(0, users.length), place, rep);
 }
 
-function genList(bot, msg, type, cred, list) {
+function genList(bot, msg, type, cred, list, userPlace, userCred) {
 
     var names = [];
 
     bot.fetchUser(list[0][0]).then(function(user) {
         names[0] = user.username;
         if (list.length == 1) {
-            pushList(bot, msg, type, cred, names, list);
+            pushList(bot, msg, type, cred, names, list, userPlace, userCred);
             return;
         }
         bot.fetchUser(list[1][0]).then(function(user) {
             names[1] = user.username;
             if (list.length == 2) {
-                pushList(bot, msg, type, cred, names, list);
+                pushList(bot, msg, type, cred, names, list, userPlace, userCred);
                 return;
             }
             bot.fetchUser(list[2][0]).then(function(user) {
                 names[2] = user.username;
                 if (list.length == 3) {
-                    pushList(bot, msg, type, cred, names, list);
+                    pushList(bot, msg, type, cred, names, list, userPlace, userCred);
                     return;
                 }
                 bot.fetchUser(list[3][0]).then(function(user) {
                     names[3] = user.username;
                     if (list.length == 4) {
-                        pushList(bot, msg, type, cred, names, list);
+                        pushList(bot, msg, type, cred, names, list, userPlace, userCred);
                         return;
                     }
                     bot.fetchUser(list[4][0]).then(function(user) {
                         names[4] = user.username;
                         if (list.length == 5) {
-                            pushList(bot, msg, type, cred, names, list);
+                            pushList(bot, msg, type, cred, names, list, userPlace, userCred);
                             return;
                         }
                         bot.fetchUser(list[5][0]).then(function(user) {
                             names[5] = user.username;
                             if (list.length == 6) {
-                                pushList(bot, msg, type, cred, names, list);
+                                pushList(bot, msg, type, cred, names, list, userPlace, userCred);
                                 return;
                             }
                             bot.fetchUser(list[6][0]).then(function(user) {
                                 names[6] = user.username;
                                 if (list.length == 7) {
-                                    pushList(bot, msg, type, cred, names, list);
+                                    pushList(bot, msg, type, cred, names, list, userPlace, userCred);
                                     return;
                                 }
                                 bot.fetchUser(list[7][0]).then(function(user) {
                                     names[7] = user.username;
                                     if (list.length == 8) {
-                                        pushList(bot, msg, type, cred, names, list);
+                                        pushList(bot, msg, type, cred, names, list, userPlace, userCred);
                                         return;
                                     }
                                     bot.fetchUser(list[8][0]).then(function(user) {
                                         names[8] = user.username;
                                         if (list.length == 9) {
-                                            pushList(bot, msg, type, cred, names, list);
+                                            pushList(bot, msg, type, cred, names, list, userPlace, userCred);
                                             return;
                                         }
                                         bot.fetchUser(list[9][0]).then(function(user) {
                                             names[9] = user.username;
-                                            pushList(bot, msg, type, cred, names, list);
+                                            pushList(bot, msg, type, cred, names, list, userPlace, userCred);
                                         }).catch(console.error);
                                     }).catch(console.error);
                                 }).catch(console.error);
@@ -165,7 +197,7 @@ function genList(bot, msg, type, cred, list) {
 
 }
 
-function pushList(bot, msg, type, cred, names, list) {
+function pushList(bot, msg, type, cred, names, list, userPlace, userCred) {
     //console.log(names);
     //console.log(list);
     let toSend = [];
@@ -174,11 +206,14 @@ function pushList(bot, msg, type, cred, names, list) {
     toSend.push("```ruby");
 
     for (var i = 0; i < names.length; i++) {
-    	
+
         toSend.push("[" + (i + 1) + "] " + names[i] + " | " + cred + ": " + list[i][1]);
-        if (i == names.length-1) {
-        	toSend.push("```");
-        	msg.channel.sendMessage(toSend).catch(console.error);
+        if (i == names.length - 1) {
+            toSend.push("-------------------------------------");
+            toSend.push("Your Placing:");
+            toSend.push("[" + userPlace + "] " + msg.author.username + " | " + cred + ": " + userCred);
+            toSend.push("```");
+            msg.channel.sendMessage(toSend).catch(console.error);
         }
     }
 
